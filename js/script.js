2af185e963083f42f417550ddd314ba3e06791aa
default <yunus.emre.guzel@mths.ca>
2025-05-06 14:03:17 +0000

    finshed the prgram

--- js/script.js
+++ js/script.js
@@ -1,24 +1,63 @@
-
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Emre Guzel
-// Created on: April 29 2025
-// This file contains the JS functions for index.html
-
 "use strict"
-// Setting the functoin 
-function musuemDiscountAgeCalculator() {
+// Setting the function 
+function calculateFood() {
   // Setting the variables
-  const age = parseInt(document.getElementById("age").value)
-  const day = document.getElementById("day").value
-  let message = ""
+  const spiceLevel = document.getElementById("spice-level").value
+  const kebabType = document.getElementById("kebab-type").value
+  const sizeLarge = document.getElementById("size-large").checked
+  const sizeMedium = document.getElementById("size-medium").checked
+  const sizeSmall = document.getElementById("size-small").checked
 
-  // Setting the if statement
-  if ((day == "Tuesday" || day == "Thursday") || (age > 12 && age < 21)){
-    message = "You're eligible for student pricing";
-  } else {
-    message = "You must pay the regular pricing"
+  let spiceLevelExtra = 0
+  let kebabPrice = 0
+  let sizePrice = 0
+  let resultMessage = ""
+
+  // setting the spice level prices
+  if (spiceLevel == 5) {
+    spiceLevelExtra = 3.00
+  } else if (spiceLevel == 4) {
+    spiceLevelExtra = 2.00
+  } else if (spiceLevel == 3) {
+    spiceLevelExtra = 1.00
+  } else if (spiceLevel == 2 || spiceLevel == 1) {
+    spiceLevelExtra = 0.50
   }
 
-  document.getElementById("result").innerHTML = message
+  // Setting the kebab type 
+  if (kebabType == "adana-kebab") {
+    kebabPrice = 30.99
+  } else if (kebabType == "urfa kebab") {
+    kebabPrice = 25.99
+  } else if (kebabType == "iskender-kebab") {
+    kebabPrice = 35.99
+  } else if (kebabType == "doner-kebab") {
+    kebabPrice = 20.99
+  } else if (kebabType == "lahmajun") {
+    kebabPrice = 10.99
+  }
+
+  // setting the sizes
+  if (sizeLarge === true) {
+    sizePrice = 7.99
+  } else if (sizeMedium === true) {
+    sizePrice = 5.99
+  } else if (sizeSmall === true) {
+    sizePrice = 2.99
+  }
+
+  // calculate subtotal, tax and total
+  const subtotal = kebabPrice + sizePrice + spiceLevelExtra;
+  const taxRate = 0.13
+  const tax = subtotal * taxRate
+  const total = subtotal + tax
+
+  // showing the result
+  resultMessage += "Kebab Price: $" + kebabPrice.toFixed(2) + "<br>";
+  resultMessage += "Size Price: $" + sizePrice.toFixed(2) + "<br>";
+  resultMessage += "Spice Extra: $" + spiceLevelExtra.toFixed(2) + "<br>";
+  resultMessage += "Tax (13%): $" + tax.toFixed(2) + "<br>";
+  resultMessage += "<strong>Total: $" + total.toFixed(2) + "</strong>";
+
+  document.getElementById("result").innerHTML = resultMessage;
 }
